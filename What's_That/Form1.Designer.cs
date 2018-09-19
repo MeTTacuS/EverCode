f5fb2926410a1d6d8ce64375c3b61a0129f05715
MeTTacuS <33838452+MeTTacuS@users.noreply.github.com>
2018-09-19 13:27:41 +0300

    Added 3 buttons that do not work, a live camera feed starts as soon as the program is launched

--- What's_That/Form1.Designer.cs
+++ What's_That/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace What_s_That
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonIdentify = new System.Windows.Forms.Button();
+            this.buttonPicture = new System.Windows.Forms.Button();
+            this.buttonAddFace = new System.Windows.Forms.Button();
+            this.imgCamUser = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonIdentify
@@ -41,17 +46,48 @@
             this.buttonIdentify.TabIndex = 0;
             this.buttonIdentify.Text = "Identify Face";
             this.buttonIdentify.UseVisualStyleBackColor = true;
-            this.buttonIdentify.Click += new System.EventHandler(this.button1_Click);
+            this.buttonIdentify.Click += new System.EventHandler(this.buttonIdentify_Click);
             // 
-            // Form1
+            // buttonPicture
+            // 
+            this.buttonPicture.Location = new System.Drawing.Point(65, 114);
+            this.buttonPicture.Name = "buttonPicture";
+            this.buttonPicture.Size = new System.Drawing.Size(96, 39);
+            this.buttonPicture.TabIndex = 1;
+            this.buttonPicture.Text = "Identify Picture";
+            this.buttonPicture.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddFace
+            // 
+            this.buttonAddFace.Location = new System.Drawing.Point(65, 159);
+            this.buttonAddFace.Name = "buttonAddFace";
+            this.buttonAddFace.Size = new System.Drawing.Size(96, 39);
+            this.buttonAddFace.TabIndex = 2;
+            this.buttonAddFace.Text = "Add Face";
+            this.buttonAddFace.UseVisualStyleBackColor = true;
+            // 
+            // imgCamUser
+            // 
+            this.imgCamUser.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.imgCamUser.Location = new System.Drawing.Point(213, 12);
+            this.imgCamUser.Name = "imgCamUser";
+            this.imgCamUser.Size = new System.Drawing.Size(336, 248);
+            this.imgCamUser.TabIndex = 2;
+            this.imgCamUser.TabStop = false;
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(230, 291);
+            this.ClientSize = new System.Drawing.Size(561, 311);
+            this.Controls.Add(this.imgCamUser);
+            this.Controls.Add(this.buttonAddFace);
+            this.Controls.Add(this.buttonPicture);
             this.Controls.Add(this.buttonIdentify);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainWindow";
+            this.Text = "What\'s That?";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCamUser)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,6 +96,9 @@
         #endregion
 
         private System.Windows.Forms.Button buttonIdentify;
+        private System.Windows.Forms.Button buttonPicture;
+        private System.Windows.Forms.Button buttonAddFace;
+        private Emgu.CV.UI.ImageBox imgCamUser;
     }
 }
 
