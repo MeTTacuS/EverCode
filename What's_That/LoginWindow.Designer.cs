ee440f719d03287814296532b6a29b945e888e7d
Rolldicer <like.dmt@gmail.com>
2018-10-05 17:02:43 +0300

    feat: enhance login window

--- What's_That/LoginWindow.Designer.cs
+++ What's_That/LoginWindow.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.loginButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.password_TextBox = new System.Windows.Forms.TextBox();
+            this.username_TextBox = new System.Windows.Forms.TextBox();
+            this.password_linkLabel = new System.Windows.Forms.LinkLabel();
+            this.register_linkLabel = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,22 +49,6 @@
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = true;
             this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(146, 174);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 29);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(146, 209);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(218, 29);
-            this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
@@ -95,16 +81,60 @@
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // password_TextBox
+            // 
+            this.password_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_TextBox.Location = new System.Drawing.Point(146, 210);
+            this.password_TextBox.Name = "password_TextBox";
+            this.password_TextBox.Size = new System.Drawing.Size(229, 29);
+            this.password_TextBox.TabIndex = 5;
+            this.password_TextBox.TextChanged += new System.EventHandler(this.password_TextBox_TextChanged);
+            // 
+            // username_TextBox
+            // 
+            this.username_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_TextBox.Location = new System.Drawing.Point(146, 174);
+            this.username_TextBox.Name = "username_TextBox";
+            this.username_TextBox.Size = new System.Drawing.Size(229, 29);
+            this.username_TextBox.TabIndex = 5;
+            this.username_TextBox.TextChanged += new System.EventHandler(this.username_TextBox_TextChanged);
+            // 
+            // password_linkLabel
+            // 
+            this.password_linkLabel.AutoSize = true;
+            this.password_linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_linkLabel.Location = new System.Drawing.Point(146, 307);
+            this.password_linkLabel.Name = "password_linkLabel";
+            this.password_linkLabel.Size = new System.Drawing.Size(176, 24);
+            this.password_linkLabel.TabIndex = 6;
+            this.password_linkLabel.TabStop = true;
+            this.password_linkLabel.Text = "Forgot a password?";
+            this.password_linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.password_linkLabel_LinkClicked);
+            // 
+            // register_linkLabel
+            // 
+            this.register_linkLabel.AutoSize = true;
+            this.register_linkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_linkLabel.Location = new System.Drawing.Point(146, 343);
+            this.register_linkLabel.Name = "register_linkLabel";
+            this.register_linkLabel.Size = new System.Drawing.Size(154, 24);
+            this.register_linkLabel.TabIndex = 6;
+            this.register_linkLabel.TabStop = true;
+            this.register_linkLabel.Text = "Need to register?";
+            this.register_linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.register_linkLabel_LinkClicked);
+            // 
             // LoginWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(398, 388);
+            this.Controls.Add(this.register_linkLabel);
+            this.Controls.Add(this.password_linkLabel);
+            this.Controls.Add(this.username_TextBox);
+            this.Controls.Add(this.password_TextBox);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.loginButton);
             this.Name = "LoginWindow";
             this.Text = "LoginWindow";
@@ -118,10 +148,12 @@
         #endregion
 
         private System.Windows.Forms.Button loginButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox password_TextBox;
+        private System.Windows.Forms.TextBox username_TextBox;
+        private System.Windows.Forms.LinkLabel password_linkLabel;
+        private System.Windows.Forms.LinkLabel register_linkLabel;
     }
 }
--- What's_That/LoginWindow.cs
+++ What's_That/LoginWindow.cs
@@ -17,17 +17,56 @@ namespace What_s_That
             InitializeComponent();
         }
 
+        //Enter code here for your version of username and userpassword 
+        Login login = new Login("admin", "1234");
+
         private void loginButton_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            var main_window = new MainWindow();
-            main_window.Closed += (s, args) => this.Close();
-            main_window.Show();
+
+            //define local variables from the user inputs 
+            string user = username_TextBox.Text;
+            string pass = password_TextBox.Text;
+
+            //check if eligible to be logged in 
+            if (login.IsLoggedIn(user, pass))
+            {
+                MessageBox.Show("You are logged in successfully");
+                this.Hide();
+                var main_window = new MainWindow();
+                main_window.Closed += (s, args) => this.Close();
+                main_window.Show();
+            }
+            else
+            {
+                //show default login error message 
+                MessageBox.Show("Login Error!");
+            }
+           
         }
 
         private void LoginWindow_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void username_TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_TextBox_TextChanged(object sender, EventArgs e)
+        {
+            password_TextBox.PasswordChar = '#';
+        }
+
+        private void password_linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Under development");
+        }
+
+        private void register_linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Under development");
         }
     }
 }
