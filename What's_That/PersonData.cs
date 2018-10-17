029abc7c3f6b48b3b16aa2ecb5a4293191730506
bikon <bikon@LEDAI>
2018-10-17 21:09:37 +0300

    Added extension method and struct

---
+++ What's_That/PersonData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_s_That
+{
+    struct PersonData
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
