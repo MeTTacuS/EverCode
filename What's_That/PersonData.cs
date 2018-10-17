97b2f8e746c2e30f499be4be0bfdd9b43633c897
bikon <bikon@LEDAI>
2018-10-17 21:21:51 +0300

    Added ID

--- What's_That/PersonData.cs
+++ What's_That/PersonData.cs
@@ -8,6 +8,7 @@ namespace What_s_That
 {
     struct PersonData
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
