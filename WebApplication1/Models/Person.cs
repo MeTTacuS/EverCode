8e52f15a1c321aab390f84efc7c8c02e67f2e609
Paulikas <37594823+Paulikas@users.noreply.github.com>
2018-11-26 18:08:39 +0200

    Changed controler added local database

--- WebApplication1/Models/Person.cs
+++ WebApplication1/Models/Person.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Models
 {
     public class Person
     {
-        public int id { get; set; }
-        public string name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
