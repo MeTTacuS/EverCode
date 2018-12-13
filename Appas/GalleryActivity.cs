d4212e55b84ce9e63f2e468c9dcab6e8f16950c8
Rolldicer <like.dmt@gmail.com>
2018-12-14 01:34:40 +0200

    Add image picking from galary

---
+++ Appas/GalleryActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Appas
+{
+    [Activity(Label = "GalleryActivity")]
+    public class GalleryActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.demo_layout);
+
+            var button = FindViewById<Button>(Resource.Id.mygallery);
+
+            button.Click += delegate {
+
+                var imageIntent = new Intent();
+                imageIntent.SetType("image/*");
+                imageIntent.SetAction(Intent.ActionGetContent);
+                StartActivityForResult(Intent.CreateChooser(imageIntent, "Select photo"), 0);
+            };
+        }
+
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Ok)
+            {
+
+                var imageView = FindViewById<ImageView>(Resource.Id.myphoto);
+                imageView.SetImageURI(data.Data);
+            }
+        }
+
+
+    }
+}
