0669ec484a2156b63417e4c701d3ab9905efbffe
Paulikas <37594823+Paulikas@users.noreply.github.com>
2018-12-15 20:29:09 +0200

    Added classes requered to use FaceApi

---
+++ Appas/Model/IdentifiedFace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Appas.Model
+{
+    class IdentifiedFace
+    {
+        public string faceId { get; set; }
+        public List<Candidate> candidates;
+    }
+
+    public class Candidate
+    {
+        public string personId;
+        public decimal confidence;
+    }
+}
---
+++ Appas/RecognitionHandler/FaceRecognizedEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace eLibrus.Events
+{
+    public class FaceRecognizedEvent
+    {
+        public delegate void FaceRecognizedEventHandler(object source, FaceRecognizedEventArgs e);
+    }
+
+    public class FaceRecognizedEventArgs
+    {
+        public int userID;
+        public FaceRecognizedEventArgs(int userID)
+        {
+            this.userID = userID;
+        }
+    }
+
+    public class FaceAddedEvent
+    {
+        public delegate void FaceAddedEventHandler(object source, FaceAddedEventArgs e);
+    }
+
+    public class FaceAddedEventArgs
+    {
+        public int userID;
+        public FaceAddedEventArgs(int userID)
+        {
+            this.userID = userID;
+        }
+    }
+}
