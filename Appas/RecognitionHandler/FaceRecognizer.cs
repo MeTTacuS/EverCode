ef114d22f17dab8c5bedf5ee4cd1bd9652d749c0
Paulikas <37594823+Paulikas@users.noreply.github.com>
2018-12-17 16:25:23 +0200

    Deleted personGroupID

--- Appas/RecognitionHandler/FaceRecognizer.cs
+++ Appas/RecognitionHandler/FaceRecognizer.cs
@@ -488,7 +488,7 @@ namespace Appas.RecognitionHandler
             return sb.ToString().Trim();
         }
 
-        public static async Task<bool> DeletePerson(string personGroupId, string personId)
+        public static async Task<bool> DeletePerson(string personId)
         {
             using (var client = new HttpClient())
             {
